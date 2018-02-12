c12adb0c45b5009880b1c1176f1017c81c3a984f
peiwen liu <peiwenliu@peiwens-MacBook-Pro.local>
2018-02-11 23:54:37 -0500

    first commit

---
+++ src/main/java/com/peiwen/tech/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.peiwen.tech.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void hello(){
+        System.out.println("hello");
+    }
+}
